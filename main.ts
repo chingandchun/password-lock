4435397c8b49b9f7239278e4812cd77585f8e6b8
chingandchun <79615723+chingandchun@users.noreply.github.com>
2021-02-24 18:00:08 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    entry = "" + entry + "A"
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+    entry = ""
+})
+input.onButtonPressed(Button.B, function () {
+    entry = "" + entry + "B"
+})
+let entry = ""
+let password = "BABBA"
+entry = ""
